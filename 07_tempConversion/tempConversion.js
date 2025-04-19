b6eb919ab5855e76fb8f897c2c167aca89af3ac7
ivan020 <ivevdm@gmail.com>
2025-04-19 15:29:14 +0100

    udpate

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+    result = (value - 32) * 5;
+    result /= 9;
+    return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+    result = (value * 1.8) + 32;
+    return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,47 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function(a, b) {
+    return a + b;
 
 };
 
-const power = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const factorial = function() {
-	
+const sum = function(listOfValues) {
+    let result = 0;
+    for (val of listOfValues) {
+        result += val;
+    }
+    return result;
+};
+
+const multiply = function(listOfValues) {
+    let result = 1;
+    for (val of listOfValues) {
+        result *= val;
+    }
+    return result;
+};
+
+const power = function(a, b) {
+    return Math.pow(a, b);
+
+};
+
+const factorial = function(n) {
+    let product = 1;
+    for (let i = n; i > 0; i--) {
+        product *= i;
+    }
+    return product;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
