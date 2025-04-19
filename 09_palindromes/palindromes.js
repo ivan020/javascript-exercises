1235cd9f42113607067aaf031968f17096eed6ab
ivan020 <ivevdm@gmail.com>
2025-04-19 15:41:14 +0100

    Im on fire!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
 
+const removePunctuation = function(word) {
+    var punctuationless = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    var finalString = punctuationless.replace(/\s{2,}/g, "");
+    return finalString;
+}
+
+
+const palindromes = function(string) {
+    var string = string.toLowerCase();
+    var finalString = removePunctuation(string);
+    let reveresed = finalString.split('').reverse().join('');
+    return finalString == reveresed;
 };
 
 // Do not edit below this line
